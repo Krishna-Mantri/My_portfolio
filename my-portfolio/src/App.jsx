08cb9b45f22a019f7393a54dbfa7000127027254
Krishna Mantri <krishnamantri950@gmail.com>
2026-01-03 11:47:39 +0530

    Fix import case for Hero component

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import {React,useEffect} from 'react'
-import Hero from './components/hero'
+import Hero from './components/Hero'
 import About from './components/About.jsx'
 import Skills from './components/Skills.jsx'
 import Projects from './components/Projects.jsx'
